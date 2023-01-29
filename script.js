5da42897ac15b5243abada3d8dc705672b5ac8d8
ceickmeyer <53309925+ceickmeyer@users.noreply.github.com>
2023-01-29 15:50:01 -0500

    nbew

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const exportButton = document.getElementById("export-button");
 
 exportButton.addEventListener("click", () => {
-  html2canvas(document.body, { width: 900 }).then((canvas) => {
+  html2canvas(document.body, { width: 700 }).then((canvas) => {
     const imgData = canvas.toDataURL("image/jpeg");
     const link = document.createElement("a");
     link.href = imgData;
